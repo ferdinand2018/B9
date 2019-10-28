d3fc1e757c462344c1b658b3e302dd5a52465497
ferdinand2018 <40237280+ferdinand2018@users.noreply.github.com>
2019-10-28 21:07:28 +0300

    Классы разбиты по пакетам

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,9 +1,7 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.tests;
 
-import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupCreationTests extends TestBase{
     @Test
