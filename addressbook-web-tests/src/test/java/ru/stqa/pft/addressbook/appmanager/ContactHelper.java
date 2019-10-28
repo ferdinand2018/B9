8d8b48d9434a6a62983175b8c7d85bab330574a7
ferdinand2018 <40237280+ferdinand2018@users.noreply.github.com>
2019-10-28 21:44:59 +0300

    Контакты: Создан класс ContactModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -36,4 +36,12 @@ public class ContactHelper extends HelperBase{
     public void acceptDelete() {
         wd.switchTo().alert().accept();
     }
+
+    public void initContactModification() {
+        click(By.xpath("//img[@alt='Edit']"));
+    }
+
+    public void submitContactModification() {
+        click(By.xpath("//input[22]"));
+    }
 }
