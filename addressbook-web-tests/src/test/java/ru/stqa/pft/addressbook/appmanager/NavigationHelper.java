dde1e5b5e8e41ec62388116a50be2393dd9830c5
ferdinand2018 <40237280+ferdinand2018@users.noreply.github.com>
2019-10-28 22:24:40 +0300

    "Ленивые" действия

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -9,7 +9,13 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void gotoGroupPage() {
-        click(By.linkText("groups"));
+        if(isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))){
+            return;
+        }else{
+            click(By.linkText("groups"));
+        }
     }
 
     public void gotoContactCreationPage() {
@@ -17,6 +23,10 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void gotoHomePage(){
-        click(By.linkText("home"));
+        if(isElementPresent(By.name("maintable"))){
+            return;
+        }else{
+            click(By.linkText("home"));
+        }
     }
 }
