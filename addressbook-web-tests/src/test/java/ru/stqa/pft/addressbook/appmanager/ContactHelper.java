d436d2b7abc9b935c839250bcf8edff8dec4d7be
ferdinand2018 <40237280+ferdinand2018@users.noreply.github.com>
2019-11-05 21:10:39 +0300

    Задание №9: Реализовать проверки путём сравнения списков

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -3,9 +3,13 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactHelper extends HelperBase{
     public ContactHelper(WebDriver wd) {
@@ -34,8 +38,8 @@ public class ContactHelper extends HelperBase{
         }
     }
 
-    public void selectContact() {
-        click(By.name("selected[]"));
+    public void selectContact(int index){
+        wd.findElements(By.name("selected[]")).get(index).click();
     }
 
     public void deleteContact() {
@@ -46,8 +50,9 @@ public class ContactHelper extends HelperBase{
         wd.switchTo().alert().accept();
     }
 
-    public void initContactModification() {
-        click(By.xpath("//img[@alt='Edit']"));
+    public int initContactModification(int i) {
+        click(By.xpath("(//img[@alt='Edit'])[" + i + "]"));
+        return i;
     }
 
     public void submitContactModification() {
@@ -55,12 +60,33 @@ public class ContactHelper extends HelperBase{
     }
 
     public void createContact(ContactData contact, boolean b) {
+        initContactCreation();
         fillContactForm(contact, true);
         submitContactCreation();
         returnToContactPage();
     }
 
+    public void initContactCreation() {
+        click(By.linkText("add new"));
+    }
+
     public boolean isThereAContact() {
         return isElementPresent(By.name("selected[]"));
+    }
+
+    public int getContactCount() {
+        return wd.findElements(By.name("selected[]")).size();
+    }
+
+    public List<ContactData> getContactList() {
+        List<ContactData> contacts = new ArrayList<ContactData>();
+        List<WebElement> elements = wd.findElements(By.name("entry"));
+        for(WebElement element : elements){
+            String firstname = element.findElement(By.xpath(".//td[3]")).getText();
+            String lastname = element.findElement(By.xpath(".//td[2]")).getText();
+            ContactData contact = new ContactData(firstname, null, lastname, null, null, null);
+            contacts.add(contact);
+        }
+        return contacts;
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,19 +1,35 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactModificationTests extends TestBase{
     @Test
     public void testContactModification() throws Exception {
         app.getNavigationHelper().gotoHomePage();
-        if(!app.getContactHelper().isThereAContact()){
-            app.getNavigationHelper().gotoContactCreationPage();
-            app.getContactHelper().createContact(new ContactData("firstname", "middlename", "lastname", "123", null, "test1"), true);
+        ContactData contact = new ContactData("Иван", "Иванович", "Иванов", null, null, "test1");
+        if (!app.getContactHelper().isThereAContact()) {
+            app.getContactHelper().createContact(contact, true);
         }
-        app.getContactHelper().initContactModification();
-        app.getContactHelper().fillContactForm(new ContactData("firstname", "middlename", "lastname", "123", "321", null), false); //Заполнение данных о контакте
+        List<ContactData> before = app.getContactHelper().getContactList();
+        app.getContactHelper().initContactModification(before.size());
+        ContactData contact1 = new ContactData("Иван", "Иванович", "Иванов", null, null, null);
+        app.getContactHelper().fillContactForm(contact1, false);
         app.getContactHelper().submitContactModification();
+        app.getContactHelper().returnToContactPage();
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(contact);
+        Comparator<? super ContactData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 }
