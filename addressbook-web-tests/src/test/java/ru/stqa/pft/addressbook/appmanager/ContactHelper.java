138c27159b03eb749f5999a9d3b8cc0ca50cb0c2
ferdinand2018 <40237280+ferdinand2018@users.noreply.github.com>
2019-11-11 21:26:39 +0300

    Задание №11: Реализовать тест для проверки информации о контактах на главной странице

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -10,7 +10,9 @@ import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ContactHelper extends HelperBase{
     public ContactHelper(WebDriver wd) {
@@ -132,5 +134,55 @@ public class ContactHelper extends HelperBase{
             contacts.add(new ContactData().withId(id).withFirstname(firstname).withLastname(lastname));
         }
         return contacts;
+    }
+
+    public Set<ContactData> allMerge() {
+        Set<ContactData> contacts = new HashSet<ContactData>();
+        List<WebElement> rows = wd.findElements(By.name("entry"));
+        for (WebElement row : rows) {
+            List<WebElement> cells = row.findElements(By.tagName("td"));
+            int id = Integer.parseInt(cells.get(0).findElement(By.tagName("input")).getAttribute("value"));
+            String lastname = cells.get(1).getText();
+            String firstname = cells.get(2).getText();
+            String allPhone = cells.get(5).getText();
+            String allEmail = cells.get(4).getText();
+            String address = cells.get(3).getText();
+            contacts.add(new ContactData()
+                    .withId(id)
+                    .withLastname(lastname)
+                    .withFirstname(firstname)
+                    .withAllPhones(allPhone)
+                    .withAllEmails(allEmail)
+                    .withAddress(address));
+        }
+        return contacts;
+    }
+
+    public ContactData  infoFromEditForm(ContactData contact){
+        initContactModificationById(contact.getId());
+        String firstname = wd.findElement(By.name("firstname")).getAttribute("value");
+        String lastname = wd.findElement(By.name("lastname")).getAttribute("value");
+        String home = wd.findElement(By.name("home")).getAttribute("value");
+        String mobile = wd.findElement(By.name("mobile")).getAttribute("value");
+        String work = wd.findElement(By.name("work")).getAttribute("value");
+        String email = wd.findElement(By.name("email")).getAttribute("value");
+        String email2 = wd.findElement(By.name("email2")).getAttribute("value");
+        String email3 = wd.findElement(By.name("email3")).getAttribute("value");
+        String address = wd.findElement(By.name("address")).getAttribute("value");
+        wd.navigate().back();
+        return new ContactData()
+                .withId(contact.getId()).withLastname(lastname).withFirstname(firstname)
+                .withHome(home).withMobile(mobile).withWork(work)
+                .withEmail(email).withEmail2(email2).withEmail3(email3).withAddress(address);
+    }
+
+    private void initContactModificationById(int id) {
+        WebElement checkbox = wd.findElement(By.cssSelector(String.format("input[value='%s']", id)));
+        WebElement row = checkbox.findElement(By.xpath("./../.."));
+        List<WebElement> cells = row.findElements(By.tagName("td"));
+        cells.get(7).findElement(By.tagName("a")).click();
+
+        //wd.findElement(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img")).click();
+        //wd.findElement(By.xpath("//input[value = '%s']/../../td[8]/a")).click();
     }
 }
