f7bf38b3477910ac74bf194b65ba30e3719466b5
ferdinand2018 <40237280+ferdinand2018@users.noreply.github.com>
2019-12-08 22:14:11 +0300

    тесты для добавления контакта в группу и удаления контакта из группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddToGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddToGroupTests.java
@@ -1,13 +1,29 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class ContactAddToGroupTests extends TestBase{
+  @BeforeMethod
+  public void ensurePreconditions(){
+    if(app.db().contacts().size() == 0) {
+      app.goTo().homePage();
+      app.contact().create(new ContactData().withFirstname("Иван").withLastname("Иванов"), true);
+    }
+  }
   @Test
   public void testContactAddToGroup() throws Exception {
+    Contacts before = app.db().contacts();
     app.appremove().selectContactById();
     app.appremove().selectGroup();
     app.appremove().addToGroup();
     app.appremove().returnToContactPage();
+    Contacts after = app.db().contacts();
+    Assert.assertEquals(after.size(), before.size());
   }
 }
